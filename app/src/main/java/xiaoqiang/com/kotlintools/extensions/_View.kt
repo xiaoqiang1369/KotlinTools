23b1136ec389f6eea0939ebedafa11c3a91c8eea
chenrenxiang <xiaoqiang1369@gmail.com>
2020-05-21 10:46:32 +0800

    removed ViewGroup extensions

--- app/src/main/java/xiaoqiang/com/kotlintools/extensions/_View.kt
+++ app/src/main/java/xiaoqiang/com/kotlintools/extensions/_View.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 
 /**
  * created by crx in 2020/5/18 11:51
- * description: set corner and stroke for View & ViewGroup
+ * description: set corner and stroke for View
  */
 
 fun View.setCorner(radius: Float) {
@@ -17,6 +17,13 @@ fun View.setCorner(radius: Float) {
     if (bgColor != 0) {
         gd.setColor(bgColor)
     }
+    gd.cornerRadius = radius
+    background = gd
+}
+
+fun View.setCorner(@ColorInt bgColor: Int, radius: Float){
+    val gd = GradientDrawable()
+    gd.setColor(bgColor)
     gd.cornerRadius = radius
     background = gd
 }
@@ -31,23 +38,11 @@ fun View.setStroke(@ColorInt color: Int, strokeWidth: Int) {
     background = gd;
 }
 
-fun ViewGroup.setCorner(radius: Float) {
+fun View.setStroke(@ColorInt strokeColor: Int, strokeWidth: Int, @ColorInt bgColor: Int, radius: Float = 0f) {
     val gd = GradientDrawable()
-    val bgColor = if (background is ColorDrawable) (background as ColorDrawable).color else 0
-    if (bgColor != 0) {
-        gd.setColor(bgColor)
-    }
-    gd.cornerRadius = radius
-    background = gd
-}
-
-fun ViewGroup.setStroke(@ColorInt color: Int, strokeWidth: Int) {
-    val gd = GradientDrawable()
-    val bgColor = if (background is ColorDrawable) (background as ColorDrawable).color else 0
-    if (bgColor != 0) {
-        gd.setColor(bgColor)
-    }
-    gd.setStroke(strokeWidth, color)
+    gd.setColor(bgColor)
+    gd.setStroke(strokeWidth, strokeColor)
+    gd.cornerRadius = radius;
     background = gd;
 }
 
